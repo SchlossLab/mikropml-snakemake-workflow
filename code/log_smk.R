960b19a31398dc1a95e0d57aa347bec804dc48c8
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-08 15:19:17 -0400

    Don't need devtools::load_all

--- code/log_smk.R
+++ code/log_smk.R
@@ -1,4 +1,3 @@
-#devtools::load_all() # TODO delete this line after pkg passes devtools check again
 if (!is.null(snakemake@log)) {
     log_filepath <- snakemake@log[1][[1]]
     log <- file(log_filepath, open = "wt")
