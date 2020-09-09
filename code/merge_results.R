b89cd51dd1d73fd8bbdd2df33cd1253ffdcadaaf
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-09 13:19:01 -0400

    Fix typo purr -> purrr

--- code/merge_results.R
+++ code/merge_results.R
@@ -1,6 +1,6 @@
 source('code/log_smk.R')
 library(dplyr)
 snakemake@input[['csv']] %>%
-  purr::map(readr::read_tsv) %>%
+  purrr::map(readr::read_tsv) %>%
   dplyr::bind_rows() %>%
   readr::write_tsv(snakemake@output[['csv']])
