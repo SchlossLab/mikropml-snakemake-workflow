3f89141c7f3e1030dfdcb972b2bfe950ea75c454
Kelly Sovacool <kellysovacool@gmail.com>
2020-10-02 12:16:38 -0400

    can't use pipe with ggsave

--- code/plot_perf.R
+++ code/plot_perf.R
@@ -1,6 +1,6 @@
 source('code/log_smk.R')
 library(dplyr)
-snakemake@input[['csv']] %>%
+perf_plot <- snakemake@input[['csv']] %>%
   readr::read_csv() %>%
-  mikropml::plot_performance() %>%
-  ggplot2::ggsave(snakemake@output[['png']])
+  mikropml::plot_performance()
+ggplot2::ggsave(snakemake@output[['png']], plot = perf_plot)
