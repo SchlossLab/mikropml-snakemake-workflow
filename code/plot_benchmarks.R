38e1cc869f236874ff9bcb9770e0a1ac7316510a
Kelly Sovacool <kellysovacool@gmail.com>
2020-10-09 09:46:06 -0400

    Uncomment lines

--- code/plot_benchmarks.R
+++ code/plot_benchmarks.R
@@ -1,15 +1,14 @@
 source("code/log_smk.R")
 library(tidyverse)
 
-dat <- read_csv('results/benchmarks_results.csv') %>%
+dat <- read_csv(snakemake@input[['csv']]) %>%
   mutate(runtime_mins = s / 60,
          memory_gb = max_rss / 1024) %>%
   select(method, runtime_mins, memory_gb) %>%
   pivot_longer(-method, names_to = 'metric') %>%
   group_by(method)
 
-#runtime_plot <- read_csv(snakemake@input[['csv']]) %>%
-dat %>%
+runtime_plot <- dat %>%
   ggplot(aes(method, value)) +
   geom_boxplot() +
   facet_wrap(metric ~ ., scales = 'free') +
