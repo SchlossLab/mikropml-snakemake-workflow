993571a962dd0a9761096a027991983e182aadaf
Kelly Sovacool <kellysovacool@gmail.com>
2020-11-13 21:18:44 -0500

    Plot model performance

--- code/plot_perf.R
+++ code/plot_perf.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 perf_plot <- snakemake@input[["csv"]] %>%
   read_csv() %>%
-  mikropml::plot_performance() +
+  mikropml::plot_model_performance() +
   theme_classic() +
   scale_color_brewer(palette = "Dark2") +
   coord_flip()
