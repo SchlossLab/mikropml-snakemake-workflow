0021f089c84f03ab721cad34ecdc85ed0c14c941
Kelly Sovacool <kellysovacool@gmail.com>
2021-04-08 15:48:29 -0400

    Load mikropml with library() in preprocess script
    Sometimes preprocess_data() throws an error when calling
    caret::dummyVars():
    
    Error in get(ctr, mode = "function", envir = parent.frame()) :
      object 'contr.ltfr' of mode 'function' was not found
    Calls: <Anonymous> ... model.matrix -> model.matrix.default -> contrasts -> get
    
    This error happens sometimes when using categorical features,
    but not for all datasets with categorical features.
    It happened to Jay with her CRC datasets split by condition when the
    plate ID column was included as a feature.
    
    For some reason, loading either mikropml or caret with library() in
    scripts fixes the problem.

--- code/preproc.R
+++ code/preproc.R
@@ -1,8 +1,10 @@
 source("code/log_smk.R")
+library(mikropml)
+
 doFuture::registerDoFuture()
 future::plan(future::multicore, workers = snakemake@resources[["ncores"]])
 
 data_raw <- readr::read_csv(snakemake@input[["csv"]])
-data_processed <- mikropml::preprocess_data(data_raw, outcome_colname = "dx")
+data_processed <- preprocess_data(data_raw, outcome_colname = "dx")
 
 saveRDS(data_processed, file = snakemake@output[["rds"]])
