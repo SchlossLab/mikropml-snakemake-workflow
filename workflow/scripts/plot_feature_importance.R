d119995ba331ef271c356dbb097c923945dba81f
Kelly Sovacool <kellysovacool@gmail.com>
2022-11-02 15:14:04 -0400

    Create example feature importance plot

--- workflow/scripts/plot_feature_importance.R
+++ workflow/scripts/plot_feature_importance.R
@@ -1,8 +1,25 @@
-# TODO: basic feature importance plot as an example
 schtools::log_snakemake()
+library(dplyr)
 library(ggplot2)
+library(schtools)
+
+feat_df <- read_csv(snakemake@input[['csv']])
+
+top_feats <- feat_df %>% 
+    group_by(method, names) %>% 
+    summarize(median_diff = median(perf_metric_diff)) %>% 
+    slice_min(order_by = median_diff, n = 5)
+
+feat_plot <- feat_df %>% 
+    right_join(top_feats, by = c('method','names')) %>% 
+    mutate(features = factor(names, levels = rev(unique(top_feats$names)))) %>% 
+    ggplot(aes(x = perf_metric_diff, y = features, color = method)) +
+    geom_boxplot() +
+    facet_wrap(~method) +
+    theme_sovacool()
+
 ggsave(
   filename = snakemake@output[["plot"]],
-  plot = ggplot(),
+  plot = feat_plot,
   device = "png"
 )
