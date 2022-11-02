3fa85838f8a86a8147776206b312850310dbc7b5
Kelly Sovacool <kellysovacool@gmail.com>
2022-11-02 13:34:57 -0400

    Update files to ignore & clean

--- workflow/scripts/clean.sh
+++ workflow/scripts/clean.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-rm -rf report.md \
-    figures/benchmarks.png \
-    figures/performance.png \
-    figures/hp_performance_glmnet.png \
-    figures/hp_performance_parRF.png \
-    figures/hp_performance_svmRadial.png \
-    results/performance_results.csv \
-    results/benchmarks_results.csv
+rm -rf \
+    report_otu-mini-bin.md \
+    report_otu-large.md \
+    figures/otu-* \
+    results/otu-*
