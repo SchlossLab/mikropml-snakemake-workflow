43ecd9e0a23d972a59b434dc90f5c642e81c2942
Kelly Sovacool <kellysovacool@gmail.com>
2022-11-02 15:15:45 -0400

    Make the blank plot tiny

--- workflow/scripts/make_blank_plot.R
+++ workflow/scripts/make_blank_plot.R
@@ -3,6 +3,7 @@ library(ggplot2)
 message("making a blank plot")
 ggsave(
   filename = snakemake@output[["plot"]],
-  plot = ggplot(),
+  plot = ggplot() + theme_void(),
+  height = 0.1, width = 0.1,
   device = "png"
 )
