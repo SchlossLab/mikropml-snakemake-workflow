b01ccf29b198cb0ac546a2cb43efaae713022692
Kelly Sovacool <kellysovacool@gmail.com>
2023-01-05 15:28:17 -0600

    Add all wildcards as columns

--- workflow/scripts/mutate_benchmark.R
+++ workflow/scripts/mutate_benchmark.R
@@ -1,10 +1,13 @@
 schtools::log_snakemake()
 library(tidyverse)
 
-snakemake@input[['tsv']]
-  read_tsv() %>%
-  mutate(
-      method = snakemake@wildcards[['method']],
-      seed = snakemake@wildcards[['seed']]
-  ) %>%
+wildcard_names <- snakemake@wildcards %>%
+    names() %>%
+    Filter(function(x) {nchar(x) > 0}, .)
+wildcards <- snakemake@wildcards[wildcard_names] %>%
+    as_tibble() %>%
+    mutate(seed = as.numeric(seed))
+
+read_tsv(snakemake@input[['tsv']]) %>%
+  bind_cols(wildcards) %>%
   write_csv(snakemake@output[['csv']])
