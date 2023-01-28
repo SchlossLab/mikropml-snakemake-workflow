50800e275c69ca707fdbc5f13d7707c11da3dd00
Kelly Sovacool <kellysovacool@gmail.com>
2023-01-28 18:42:33 -0500

    Write get_paramspace_from_config()
    Also fix instances_drop_wildcard() so it returns a unique set

--- workflow/rules/config.smk
+++ workflow/rules/config.smk
@@ -16,6 +16,14 @@ outcome_colname = config["outcome_colname"] if "outcome_colname" in config else 
 nseeds = config["nseeds"] if 'nseeds' in config else 1
 start_seed = 100
 seeds = range(start_seed, start_seed + nseeds)
+config['seed'] = list(seeds)
 
 hyperparams = config["hyperparams"] if "hyperparams" in config else None
 find_feature_importance = config["find_feature_importance"] if "find_feature_importance" in config else None
+
+# parameter space based on configfile
+paramspace = get_paramspace_from_config(config)
+# wildcard pattern without seed. needed for rule `combine_hp_performance`
+wildcard_no_seed = pattern_drop_wildcard(paramspace, 'seed')
+# wildcard pattern with all wildcards _except_ seed having double curly braces for use by `expand()` in rule `combine_hp_performance`
+wildcard_tame_seed = pattern_tame_wildcard(paramspace, 'seed')
--- workflow/scripts/test_functions.py
+++ workflow/scripts/test_functions.py
@@ -18,6 +18,25 @@ def test_pattern_drop_wildcard():
     assert pattern_drop_wildcard(params, 'ml_method') == 'dataset-{dataset}/seed-{seed}'
 
 def test_instances_drop_wildcard():
-    assert instances_drop_wildcard(params, 'seed') == ['dataset-otu_large/ml_method-glmnet', 'dataset-otu_large/ml_method-rf']
-    assert instances_drop_wildcard(params, 'dataset') == ['seed-0/ml_method-glmnet', 'seed-1/ml_method-rf']
-    assert instances_drop_wildcard(params, 'ml_method') == ['dataset-otu_large/seed-0', 'dataset-otu_large/seed-1']
+    assert instances_drop_wildcard(params, 'seed') == {'dataset-otu_large/ml_method-glmnet', 'dataset-otu_large/ml_method-rf'}
+    assert instances_drop_wildcard(params, 'dataset') == {'seed-0/ml_method-glmnet', 'seed-1/ml_method-rf'}
+    assert instances_drop_wildcard(params, 'ml_method') == {'dataset-otu_large/seed-0', 'dataset-otu_large/seed-1'}
+
+    conf2 = {'dataset': 'otu_large', 
+        'seed': list(range(2)),
+        'kfold': 5,
+        'ml_method': ['glmnet', 'rf']}
+    p2 = get_paramspace_from_config(conf2)
+    assert instances_drop_wildcard(p2, 'seed') == {'dataset-otu_large/kfold-5/ml_method-glmnet', 'dataset-otu_large/kfold-5/ml_method-rf'}
+
+def test_get_paramspace_from_config():
+    config = {'dataset': 'otu_large', 
+              'exclude_param_keys': ['exclude_param_keys'],
+              'seed': list(range(4)),
+              'ml_method': ['glmnet', 'rf']}
+    p = get_paramspace_from_config(config)
+    assert p.wildcard_pattern == 'dataset-{dataset}/ml_method-{ml_method}/seed-{seed}'
+    assert list(p.instance_patterns) == ['dataset-otu_large/ml_method-glmnet/seed-0', 'dataset-otu_large/ml_method-rf/seed-0', 'dataset-otu_large/ml_method-glmnet/seed-1', 'dataset-otu_large/ml_method-rf/seed-1', 'dataset-otu_large/ml_method-glmnet/seed-2', 'dataset-otu_large/ml_method-rf/seed-2', 'dataset-otu_large/ml_method-glmnet/seed-3', 'dataset-otu_large/ml_method-rf/seed-3']
+    
+def test_set_default():
+    pass # TODO
